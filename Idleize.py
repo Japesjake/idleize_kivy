f012256c6e4acd878022d1c31d2abe8ff36997fc
John Paul Jaquith <japesjake@gmail.com>
2026-03-28 01:28:24 -0400

    added increments and saving while exiting.

--- Idleize.py
+++ Idleize.py
@@ -4,11 +4,17 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import NoTransition
 from kivy.properties import NumericProperty
 from kivy.clock import Clock
+import subprocess, sys, pickle
+from pathlib import Path
 
 Builder.load_file('main.kv')
 
 class MainLayout(BoxLayout):
-    copper = NumericProperty(0)
+    path = Path("copper.p")
+    if path.is_file():
+        with open('copper.p', 'rb') as f:
+            copper = NumericProperty(pickle.load(f))
+    else: copper = NumericProperty(0)
     rate = 1
     running = False
     def idle(self):
@@ -17,16 +23,22 @@ class MainLayout(BoxLayout):
             self.running = True
         else: 
             self.running_clock.cancel()
+###
             self.running = False
-
-    def increment(self, inc, ):
+    def increment(self, inc):
         self.copper += 1
         print(self.copper)
-            
+    def on_stop(self):
+        # self.running_clock.cancel()
+        with open('copper.p', 'wb') as f: 
+            pickle.dump(self.copper, f)
 
 class Idleize(App):
     def build(self):
-        return MainLayout()
-
+        self.main = MainLayout()
+        return self.main
+    def on_stop(self):
+        with open('copper.p', 'wb') as f: 
+            pickle.dump(self.main.copper, f)
 app = Idleize()
 app.run()
