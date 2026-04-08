b8392b03047fc6ded143b51990bf18b0a272926e
John Paul Jaquith <japesjake@gmail.com>
2026-04-07 20:21:43 -0400

    successfully made it so an idling thread doesnt get created if there is already a thread with the same username as the client.

--- server.py
+++ server.py
@@ -3,15 +3,15 @@ import threading, queue, time
 
 q = queue.Queue()
 host = '0.0.0.0'
-port = 1234
+port = 1235
 idle_threads = []
 connections = []
 class Server():
     def __init__(self):
-        self.addr = None
+        pass
     def handle_client(self, conn, addr, username):
         """Function to handle individual client communication."""
-        print(f"[NEW CONNECTION] {self.addr} connected.")
+        print(f"[NEW CONNECTION] {addr} connected.")
         try:
             while True:
                 # Receive data (blocking call)
@@ -23,7 +23,7 @@ class Server():
 
                 q.put(data)
                 
-                print(f"[{self.addr}] says: {data}")
+                print(f"[{addr}] says: {data}")
                 
                 # Echo back to client
                 conn.send(f"Server received: {data}".encode('utf-8'))
@@ -31,9 +31,8 @@ class Server():
             pass
         finally:
             conn.close()
-            print(f"[DISCONNECTED] {self.addr} closed.")
-
-    def start(self):
+            print(f"[DISCONNECTED] {addr} closed.")
+    def start_server(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((host, port))
         server.listen()
@@ -41,28 +40,41 @@ class Server():
         while True:
             
             # Wait for a new connection (blocking call)
-            self.conn, self.addr = server.accept()
+            conn, addr = server.accept()
             ####
             # username = conn.recv(1024).decode('uft-8')
             username = 'JpJab'
             ####
-            
-            # Start a new thread for each client
-
-            connection = threading.Thread(target=self.handle_client, args=(self.conn, self.addr, username))
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr, username))
+            connection = Connection(conn, addr, username, thread)
             connections.append(connection)
-            connection.start()
+            connection.thread.start()
+
             conflict = False
+            print(connections)
+            print(idle_threads)
             for connection in connections:
+                print(connection.username)
                 for idle_thread in idle_threads:
+                    print(idle_thread.username)
                     if connection.username == idle_thread.username:
                         conflict = True
+                        print('conflict !!!!!')
             if not conflict:
                 idle_thread = Idle_thread(username)
+                idle_threads.append(idle_thread)
                 idle_thread.start_idler()
                 print(f"[ACTIVE THREADS] {threading.active_count() - 1}")
 
-class Idle_thread(threading.Thread):
+class Connection():
+    def __init__(self, conn, addr, username, thread):
+        self.conn = conn
+        self.addr = addr
+        self.username = username
+        self.thread = thread
+
+
+class Idle_thread():
     def __init__(self, username):
         self.idling = False
         self.username = username
@@ -81,11 +93,9 @@ class Idle_thread(threading.Thread):
                     break
     def start_idler(self):
         idle_thread = threading.Thread(target=self.idle_loop, args=(self.username,))
-        # print(f"idle thread.username = {idle_thread.username}")
-        idle_threads.append(idle_thread)
         idle_thread.start()
         print(f"idle thread created.")
 
 if __name__ == "__main__":
     server = Server()
-    server.start()
+    server.start_server()
